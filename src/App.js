360d18cdd361008efa83ac6aa3313e292c4a5b5e
arpan-jo <arpanjorng@gamil.com>
2021-03-20 14:05:49 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { createContext, useState } from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Destinations from './components/Destination/Destinations';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import NoMatch from './components/NoMatch/NoMatch';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const UserContext = createContext();
+const App = () => {
+    const [loggenInUser, setLoggedInUser] = useState({});
+    return (
+        <div className="main">
+            <div className="container">
+            <UserContext.Provider value={[loggenInUser, setLoggedInUser]}>
+                <Router>
+                    <Header/>
+                    <Switch>
+                        <Route exact path="/login">
+                            <Login/>
+                        </Route>
+                        <Route exact path="/signup">
+                            <Signup/>
+                        </Route>
+                        <PrivateRoute exact path="/:vehicle">
+                            <Destinations/>
+                        </PrivateRoute>
+                        <PrivateRoute exact path="/destination">
+                            <Destinations/>
+                        </PrivateRoute>
+                        <Route exact path="/">
+                            <Home/>
+                        </Route>
+                        <Route exact path="*">
+                            <NoMatch/>
+                        </Route>
+                    </Switch>
+                </Router>
+            </UserContext.Provider>
+            </div>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/components/AuthConfig/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+   apiKey: 'AIzaSyCo_YdIBs6qkQhrvjOOGijH6idysEExwBg',
+   authDomain: 'eazy-ride.firebaseapp.com',
+   projectId: 'eazy-ride',
+   storageBucket: 'eazy-ride.appspot.com',
+   messagingSenderId: '841302223055',
+   appId: '1:841302223055:web:4bfa938779b8735831afba',
+};
+export default firebaseConfig;
